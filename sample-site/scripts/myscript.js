72303c7c6679fd370033a0a3a917ba0636938a2f
daniel-derhami <shahriarderhami@gmail.com>
2023-02-08 23:21:34 +0100

    Continue with js

--- sample-site/scripts/myscript.js
+++ sample-site/scripts/myscript.js
@@ -2,3 +2,10 @@
 
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'be kind';
+
+/* Simple event test
+document.querySelector('html').onclick = function(){
+alert ('oops!');
+}
+*/
+
